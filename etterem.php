3212a48731b63073ae048d58209b95d34ab25923
Adammihaly <adammihaly05@gmail.com>
2023-12-20 08:41:17 +0100

    Foglalas kesz
    Foglalas kesz

--- etterem.php
+++ etterem.php
@@ -578,7 +578,7 @@ if (isset($_GET['f'])) {
             <div class="fog_conn">
                 <h2>Foglaltsági táblázat</h2>
                 <p>Válassz ki egy dátumot a lentiekben és nézd meg, hogy vajon az adott napon melyik időpontokban foglalt a megadott lakásétterem. A dátum kiválasztása után nyomj a megtekintés gombara. Ezt követően rendszerünk ki fogja írni melyik időpontak a foglaltak. Ha nincs az adott napon foglalt időpont, akkor azt fogja írni, hogy "Nem található az adott napon aktív foglalás!".</p>
-                <form action="./etterem.php" method="GET">
+                <form action="php/etteremdatum.php" method="POST">
                     <label for="datum">Válassz dátumot</label>
                     <input type="date" name="datum" required>
                     <button name="sub">Megtekintés</button>
@@ -591,11 +591,11 @@ if (isset($_GET['f'])) {
                 if (isset($_GET['fdatum'])) {
                     $foglalas_datum = $_GET['fdatum'];
 
-                    echo "<h2>Foglalt időpontok a kiválasztott napon:</h2><p>";
+                    echo "<h2>Foglalt időpontok a kiválasztott napon:</h2><p style='color: red;'>";
 
                         $sql = "SELECT * FROM foglalasok WHERE etteremID = $ID AND datum = '$foglalas_datum'";
                             $result = $conn->query($sql);
-                            if ($result->fetch_assoc() != null) {
+                            if ($result->num_rows > 0) {
                                      
                                 while ($row = $result->fetch_assoc()) 
                                 {
