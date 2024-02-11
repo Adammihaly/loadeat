7117142b2db9eeb6274a7ebd6726eaeefc970b96
Szilagyi Roland <szilagyir206@proton.me>
2024-02-11 15:21:57 +0100

    Kesz
    minden mukodik

---
+++ js/fogadok_lakasetterm.js
@@ -0,0 +1,7 @@
+document.getElementById('fogadok').addEventListener('click', () =>{
+    window.location.href ='fogadok.html';
+})
+
+document.getElementById('lakasettermek').addEventListener('click', () => {
+    window.location.href = 'index.php';
+})
