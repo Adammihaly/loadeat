4b5d64d7dd89a39ea7325e295ebb57c47ccc5bd0
Szilagyi Roland <szilagyir206@proton.me>
2024-02-15 11:59:03 +0100

    Kesz az egesz. responsivitas kell meg

--- js/fogadok_hozzaadasa.js
+++ js/fogadok_hozzaadasa.js
@@ -3,15 +3,16 @@ const masodik = document.getElementById("2");
 const harmadik = document.getElementById("3");
 const negyedik = document.getElementById("4");
 const otodik = document.getElementById("5");
-const hatodik = document.getElementById("6");
-const hetedik = document.getElementById("7");
+
 var szam = 0;
 const feladat_szama = document.getElementById("oldal_szam")
 
-const tomb = [elso, masodik, harmadik, negyedik,otodik,hatodik,hetedik];
+const tomb = [elso, masodik, harmadik, negyedik,otodik];
 
 const elore_gomb = document.getElementById("elore");
 const hatra_gomb = document.getElementById("hatra");
+const submit = document.getElementById("submit");
+submit.style.display = 'none'
 
 function szamlalo()
 {
@@ -37,6 +38,17 @@ function szamlalo()
     hatra_gomb.disabled = szam === 0;
     elore_gomb.disabled = szam === tomb.length - 1;
 
+    if(szam === tomb.length-1)
+    {
+        elore_gomb.style.display = 'none';
+        submit.style.display = 'block';
+    }
+    else
+    {
+        elore_gomb.style.display = 'block';
+        submit.style.display = 'none';
+    }
+
 }
 
 elore_gomb.onclick = function ()
@@ -48,10 +60,27 @@ elore_gomb.onclick = function ()
 }
 
 hatra_gomb.onclick = function (){
+   
     szam--;
     feladat_szama.textContent = szam +1;
     szamlalo();
 
 }
+
+function validateForm() {
+
+    var form = document.querySelector('form');
+    var elements = form.elements;
+
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].hasAttribute('required') && elements[i].value.trim() === '') {
+            alert("A(z) " + elements[i].name + " nincs kitöltve. Kérlek töltsd ki a csillagozott mezőket!");
+            return false;
+        }
+    }
+
+    return true;
+}
+
 
 szamlalo();
